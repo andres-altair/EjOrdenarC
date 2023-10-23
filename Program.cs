ed8744e36ed476e92fc18593734152e4ac62ec9f
LAPTOP-8697QDAE\andre <csi23-amoliaz@colegioaltair.net>
2023-10-23 17:31:27 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using EjerOrdenarC;
+namespace EjerOrdenarC
+{
+    class program
+    {
+        static public void Main(string[] args)
+        {
+            int i, j, k;
+            i = solicitarNumero();
+            j = solicitarNumero();
+            k = solicitarNumero();
+
+            if (i >= j & i >= k)
+            {
+                if (j >= k)
+                {
+                    Console.WriteLine("Orden: " + i + j + k);
+                }
+                else
+                {
+                    Console.WriteLine("Orden: " + i + k + j);
+                }
+            }
+            else if (j >= i & j >= k)
+            {
+                if (i >= k)
+                {
+                    Console.WriteLine("Orden: " + j + i + k);
+                }
+                else
+                {
+                    Console.WriteLine("Orden: " + j + k + i);
+                }
+            }
+            else if (k >= i & k >= j)
+            {
+                if (i >= j)
+                {
+                    Console.WriteLine("Orden: " + k + i + j);
+                }
+                else
+                {
+                    Console.WriteLine("Orden: " + k + j + i);
+                }
+            }
+
+        }
+
+        static private int solicitarNumero()
+        {
+            int numero;
+
+            Console.WriteLine("Escriba un número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            return numero;
+        }
+    }
+}
+    
